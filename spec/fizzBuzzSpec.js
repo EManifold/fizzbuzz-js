e6f7600858567343267675f519bf537e807b1d31
bensheridanedwards <bensheridanedwards@gmail.com>
2019-12-09 16:59:37 +0000

    Implemented first unit test for fizzbuzz

---
+++ spec/fizzBuzzSpec.js
@@ -0,0 +1,5 @@
+describe("FizzBuzz", function() {
+  it("should return a fizz when passed 3", function() {
+    expect(fizzBuzz(3)).toEqual("Fizz");
+  })
+})
---
+++ src/fizzBuzz.js
@@ -0,0 +1,6 @@
+
+
+function fizzBuzz(number) {
+  return "Fizz"
+};
+
